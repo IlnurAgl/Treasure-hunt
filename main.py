f01c510bc8b7ffadbf0d1b50b19dffc650cf170f
Ilnur <ilnur5247@gmail.com>
2019-02-04 12:18:06 +0300

    04.02.1/12:18

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame
+import os
+import sys
 
 FPS = 60
 WIDTH = 1200
-HEIGHT = 800
+HEIGHT = 900
 RUNNING = True
 player = None
 
@@ -19,7 +21,7 @@ def load_image(name, color_key=None):
     except pygame.error as message:
         print('Cannot load image:', name)
         raise SystemExit(message)
-    image = image.convert_alpha()
+    image = image.convert()
 
     if color_key is not None:
         if color_key is -1:
@@ -32,16 +34,27 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__(player_group, all_sprites)
         self.image = player_image
-        self.rect = self.image.get_rect().move(tile_width * pos_x + 15,
-                                               tile_height * pos_y + 5)
+        self.rect = self.image.get_rect()
+        self.add(player_group)
+
+
+class Dirty(pygame.sprite.Sprite):
+    pass
+
+
+class Ground(pygame.sprite.Sprite):
+    pass
 
 
 pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 all_sprites = pygame.sprite.Group()
 dirty = pygame.sprite.Group()
 ground = pygame.sprite.Group()
-background = pygame.image.load("data/background.png")
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+player_group = pygame.sprite.Group()
+background = pygame.image.load("data/background.png").convert()
+player_image = load_image('player.png', color_key=-1)
+Player(200, 200)
 clock = pygame.time.Clock()
 
 while RUNNING:
@@ -49,6 +62,7 @@ while RUNNING:
         if event.type == pygame.QUIT:
             RUNNING = False
     screen.blit(background, [0, 0])
+    all_sprites.draw(screen)
     pygame.display.flip()
     clock.tick(FPS)
 terminate()
